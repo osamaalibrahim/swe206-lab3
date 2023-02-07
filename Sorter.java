ca823f1406f4862f7edd4fa9fe5043679be799f8
Osama Alibrahim <os778ma@gmail.com>
2023-02-07 16:45:18 +0300

    adding selection sort method

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,16 @@
 public class Sorter{
-    
+    public static void selectionSort(int[] arr){  
+        for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;//searching for lowest index  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+    }  
 }
