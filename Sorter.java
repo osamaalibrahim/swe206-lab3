35412c0e7c09d15017840c90615fddeda1640e7e
Ahmed Al Mughirah <s202018480@kfupm.edu.sa>
2023-02-07 17:07:27 +0300

    Added insertionsort

--- Sorter.java
+++ Sorter.java
@@ -1,3 +1,11 @@
 public class Sorter{
-    
+    public static <T> void insertionSort(int[] array){
+        for(int i=1 ; i < array.length ; i++ ){
+            int tmp = array[i];
+            for(int j =i-1 ; j >= 0 && tmp < array[j]; j--){
+                array[j+1] = array[j];
+            }
+            array[i] = tmp;
+        }
+    }
 }
